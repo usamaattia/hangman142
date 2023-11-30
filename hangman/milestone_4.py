c8943ef6c168cd3c609338f1b1116670044e18e0
usamaattia <usamaalatiyah9@gmail.com>
2023-11-30 12:12:26 +0000

    showing letters guessed

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -1,5 +1,5 @@
 import random
-# TODO: REVIEW - make sure you have a line space between your import statements and class definition
+
 class Hangman:
 
     def __init__(self, word_list, num_lives=5):
@@ -16,19 +16,26 @@ class Hangman:
     
     def check_guess(self, guess):
         guess = guess.lower()
-
-        if guess in self.word:
-            print(f"Good guess! {guess} is in the word.")
-
-            for i, letter in enumerate(self.word):
-                if letter == guess:
+        check_list =[]
+        
+        for string in self.word_list:
+            words = string.split()  
+            for i, word in enumerate(words):
+                if guess in word:
+                    check_list.append(True)
                     self.word_guessed[i] = guess
+                else:
+                    check_list.append(False)
+                    
 
+        if any(check_list):
+            print(f"Good guess! {guess} is in the word.")
             self.num_letters -= 1
         else:
             self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word.")
             print(f"You have {self.num_lives} lives left.")
+
 
 
     def ask_for_input(self):
@@ -39,16 +46,16 @@ class Hangman:
                 print("Invalid letter. Please, enter a single alphabetical character.")
             elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
+                print(self.list_of_guesses)
+                print(f"You have {self.num_lives} lives left.")
             else:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
                 break
 
-# TODO: REVIEW - in this game, it would be good for the player to be able to see the self.word_guessed variable each time they make a guess
-# it would also be good for them to see the list of letters they've already guess as well as the number of lives they have left even if they make a correct guess or a repeated guess.
 
-# TODO: REVIEW - you can put this code in an 'if __name__ == "__main__":' statement so that it will only run if this file is called directly, not when it is imported as a module to another file.
 #Example usage:
-# word_list = ['apple', 'banana', 'orange', 'grape']
-# hangman_game = Hangman(word_list)
-# hangman_game.ask_for_input()
+if __name__ == "__main__":
+    word_list = ['apple', 'banana', 'orange', 'grape']
+    hangman_game = Hangman(word_list)
+    hangman_game.ask_for_input()
