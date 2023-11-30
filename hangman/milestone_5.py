05b65b6993093b754794c09bd05b6922414562c1
usamaattia <usamaalatiyah9@gmail.com>
2023-11-30 12:20:56 +0000

    print the predictied word

--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -1,21 +1,19 @@
 from milestone_4 import Hangman
 
 def play_game(word_list):
-    # TODO: REVIEW - you can define 'num_lives = 6' within the brackets when your create your instance of the Hangman class on line 6
-    # This is because num_lives is a keyword argument
-    num_lives = 6
-    game = Hangman(word_list, num_lives)
+
+    game = Hangman(word_list, num_lives=6)
 
     while True:
         if game.num_lives == 0:
             print("You lost!")
-            # TODO: REVIEW - it would be good to print out the word the player was guessing so that they know the answer even though they lost
+            print(f"the word you are guessing is {game.word}")
             break
         elif game.num_letters > 0:
             game.ask_for_input()
         else:
             print("Congratulations. You won the game!")
-            # TODO: REVIEW - it would be good to print out the word the player was guessing to confirm to the player that they did guess the correct word
+            print(f"the word you are guessing is {game.word}")
             break
 
 word_list = ['apple', 'banana', 'orange', 'grape']
